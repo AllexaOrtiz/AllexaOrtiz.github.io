b94862588c77b0e7abacbaeded15b16b95a84710
AllexaOrtiz <aortiz21@students.kgi.edu>
2022-05-05 21:47:18 -0700

    Update behavior.js

--- behavior.js
+++ behavior.js
@@ -1,14 +1,19 @@
 // calling this behavior because JS controls the behavior of the website
-//Still need to figure out what general dynamics to add
 
 
-//Game behavior
+//let's add cookies here COOKIES COOKIES COOKIES COOKIES
+
+
+//let's add map stuff here! MAP MAP MAP MAP MAP MAP MAP
+
+
+//Game behavior for recoveryVR "simulation"
 //Refers to lines 39 to 51 in the recoveryVR HTML file
 //Refers to lines 41 to 60 in the CSS file
 const textElement = document.getElementById('text')
 const optionButtonsElement = document.getElementById('option-buttons')
 
-let state = {} //keeps track of what the character has on them
+let state = {} //keeps track of what items the character has on them
 
 function startGame() {
     state = {} //want to start off with the character having nothing
@@ -58,7 +63,8 @@ const textNodes = [
             },
             {
                 text: 'Go straight home', //we don't need to add the goo here
-                nextText: 2
+                setState: {athome: true},
+                nextText: 7
             }
         ]
     },
@@ -130,6 +136,133 @@ const textNodes = [
     {
         id: 6,
         text: "You overfill your stomach and get constipated due to the surgery. You're stuck on the toilet all night.",
+        options: [
+            {
+                text: 'Restart', //want to recall the start game function here
+                nextText: -1 //signifies that we want to restart the game
+            }
+        ]
+    },
+    {
+        id: 7,
+        text: "You decide to drive home and rest, after two months, you go to your appointment. You're recovery's going well!",
+        options: [
+            {
+                text: 'You decide to head back home.', //want to have new options available for eating when recovered.
+                setState: {healed: true, gotoBK: true},
+                nextText: 8 //signifies that we want to restart the game
+            }
+        ]
+    },
+    {
+        id: 8, //you're recovered and you pass by the BK again
+        text: 'On your way home, you see the drive-through Burger King again. You decide to stop by now to celebrate your recovery progress.',
+        options: [ //different options for what we can do
+            {
+                text: 'Buy a burger, fries, and milkshake.',
+                requiredState: (currentState) => (currentState.gotoBK, currentState.healed),
+                setState: {all: true, milkshake: false, fries: false}, //you're getting everything, but you've made recovery progress now
+                nextText: 9 //move onto the next text node
+            },
+            {
+                text: 'Get a milkshake only.', //
+                requiredState: (currentState) => (currentState.gotoBK, currentState.healed),
+                setState: {milkshake: true, all: false, fries: false},
+                nextText: 10
+            },
+            {
+                text: 'Get fries only.', //getting fries but healed now
+                requiredState: (currentState) => (currentState.gotoBK, currentState.healed),
+                setState: {fries: true},
+                nextText: 11
+            }
+        ]
+    },
+    {
+        id: 9,
+        text: 'After leaving Burger King, you head home with your burger, milkshake and fries.',
+        options: [
+            {
+                text: 'Drink the milkshake.',
+                requiredState: (currentState) => (currentState.milkshake, currentState.healed),
+                nextText: 10
+            },
+            {
+                text: 'Eat the fries.',
+                requiredState: (currentState) => (currentState.fries, currentState.healed),
+                nextText: 11
+            },
+            {
+                text: 'Eat the burger, milkshake and fries.',
+                requiredState: (currentState) => (currentState.all, currentState.healed),
+                nextText: 12
+            }
+        ]
+    },
+    {
+        id: 10,
+        text: 'You drink the milkshake and feel fine. You continue to recover just fine with daily walks and drinking enough water.',
+        options: [
+            {
+                text: 'Restart', //want to recall the start game function here
+                nextText: -1 //signifies that we want to restart the game
+            }
+        ]
+    },
+    {
+        id: 11,
+        text: 'You eat the fries and feel a little queasy, but you get over it after an hour or two. However, the next few days you feel sluggish.',
+        options: [
+            {
+                text: 'Restart', //want to recall the start game function here
+                nextText: -1 //signifies that we want to restart the game
+            }
+        ]
+    },
+    {
+        id: 12,
+        text: 'You try to eat the burger, fries and milkshake, but you get full after half the burger and some of the shake.',
+        options: [
+            {
+                text: 'Force yourself to finish the rest.',
+                requiredState: (currentState) => (currentState.all, currentState.healed),
+                nextText: 13
+            },
+            {
+                text: 'Go for a short walk.',
+                requiredState: (currentState) => (currentState.all, currentState.healed),
+                nextText: 14
+            },
+            {
+                text: 'Give the rest to a friend or toss it.',
+                requiredState: (currentState) => (currentState.all, currentState.healed),
+                nextText: 15
+            }
+        ]
+    },
+    {
+        id: 13,
+        text: 'You start getting stomach pains so you take medication. You decide to go to sleep and come to terms with the fact that you cannot eat so much anymore.',
+        options: [
+            {
+                text: 'Restart', //want to recall the start game function here
+                nextText: -1 //signifies that we want to restart the game
+            }
+        ]
+    },
+    {
+        id: 14,
+        text: 'You go for a short walk and feel great! Still not hungry though, so you decide to toss the food and head to bed.',
+        options: [
+            {
+                text: 'Restart', //want to recall the start game function here
+                nextText: -1 //signifies that we want to restart the game
+            }
+        ]
+    },
+    {
+        id: 15,
+        text: 'You and your friend watch a movie and you fall asleep on the couch. You are a little stiff in the morning, so you do some stretches to help with that.',
         options: [
             {
                 text: 'Restart', //want to recall the start game function here
