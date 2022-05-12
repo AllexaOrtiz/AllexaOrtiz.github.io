5e6a8f2d1d53c349ef56b8fccf0b0be820e5b39a
AllexaOrtiz <aortiz21@students.kgi.edu>
2022-05-12 16:42:55 -0700

    cleaning up code

--- behavior.js
+++ behavior.js
@@ -1,17 +1,8 @@
 // calling this behavior because JS controls the behavior of the website
-
-
+//unused code will either be in the bottom or commented out of the appropriate section
 //Game behavior for recoveryVR "simulation" is in the recoveryVRbahavior.js file
 
-
-// Populating sign in fields
-// Creating array of objects
-//var information = [{'name':'Allexa Ortiz', 'email': 'aortiz21@students.kgi.edu', 'age': '23', 'condition': 'Gastric  Bypass', 'username':'aortiz', 'password':'crackers' },
-  //                 {'name':'Sophia Sy', 'email': 'ssy21@students.kgi.edu', 'age': '22', 'condition': 'Traumatic Brain Injury', 'username':'ssy', 'password':'cookies'},
-    //                ];
-
-
-//sign in cookies
+//Sign in cookies set up
 $('submit-input').click(function () {
     //Set sign in/sign up inputs
     var eaddress = $('#eaddress-input').val();
@@ -23,7 +14,6 @@ $('submit-input').click(function () {
     Cookies.set('password', password);
 
 
-// COOKIES
 //When a user save profile the profile builder form
 $('save-input').click(function () { 
     // Set profile info from inputs
@@ -31,8 +21,8 @@ $('save-input').click(function () {
     var age = $('#age-input').val();
     var email = $('#email-input').val();
     var condition = $('#condition-input').val();
-    var zipcode = $('#zipcode-input').val();
-    //We have a zipcode input, but no field in the profile builder form- ALLEXA
+    //var zipcode = $('#zipcode-input').val();
+    //There's no 'zipcode' field in the profile builder form. Will use 91711 claremont area for the maps, but will not use input to generate them- ALLEXA
 
 
 //Create profile cookie
@@ -40,20 +30,11 @@ $('save-input').click(function () {
     Cookies.set('age', age);
     Cookies.set('email', email);
     Cookies.set('condition', condition);
-    Cookies.set('zipcode', zipcode);
+    //Cookies.set('zipcode', zipcode);
 
 //Reload page so cookie can take effect
     location.reload();
 });
-
-// If there is a cookie, show welcome message
-var fullName = Cookies.get('fullName');
-if (fullName) {
-    //Hide new user input form
-    $('#new-user-input').hide();
-    //Show welcome message
-    $('#welcome-text').text('Welcome,' + fullName);
-}
 
 /* THIS IS FOR THE SIGN-IN VALIDATION */
 /* Code adapted from: https://www.youtube.com/watch?v=PkxA6m-NNCY */
@@ -76,3 +57,18 @@ function validate()
     }
 
 }
+
+// If there is a cookie, show welcome message
+//var fullName = Cookies.get('fullName');
+//if (fullName) {
+    //Hide new user input form
+  //  $('#new-user-input').hide();
+    //Show welcome message
+  //  $('#welcome-text').text('Welcome,' + fullName);
+//}
+
+// Populating sign in fields
+// Creating array of objects
+//var information = [{'name':'Allexa Ortiz', 'email': 'aortiz21@students.kgi.edu', 'age': '23', 'condition': 'Gastric  Bypass', 'username':'aortiz', 'password':'crackers' },
+  //                 {'name':'Sophia Sy', 'email': 'ssy21@students.kgi.edu', 'age': '22', 'condition': 'Traumatic Brain Injury', 'username':'ssy', 'password':'cookies'},
+    //                ]
